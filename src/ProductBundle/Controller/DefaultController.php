26dcaedd898e1dd3dfc15fef4d59341061a79206
Kornél Balatoni <blal.hu@gmail.com>
2019-09-01 10:30:17 +0200

    list products

--- src/ProductBundle/Controller/DefaultController.php
+++ src/ProductBundle/Controller/DefaultController.php
@@ -2,16 +2,34 @@
 
 namespace ProductBundle\Controller;
 
+use ProductBundle\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/api/products/")
      */
-    public function indexAction()
+    public function listAction()
     {
-        return $this->render('ProductBundle:Default:index.html.twig');
+        /**
+         * @var Product[] $products
+         */
+        $products = $this
+            ->get('doctrine.orm.entity_manager')
+            ->getRepository('ProductBundle\Entity\Product')
+            ->findAll()
+        ;
+        $returnData = [ 'products' => [] ];
+        foreach ( $products as $product ) {
+            $returnData['products'][] = [
+                'product-id'       => $product->getId(),
+                'name'             => $product->getName(),
+                'available-amount' => $product->getAvailableQuantity(),
+            ];
+        }
+        return new JsonResponse($returnData);
     }
 }
